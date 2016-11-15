27a3ebc413ae070d3ffd01fd49188445d72929c1
Liam Faulds <chaosend50@gmail.com>
2016-11-15 14:57:10 +0000

    Updated XML
    Primarily XML fixes
    Making UI work on Mobile device as opposed to Tablet.
    Had to remove Passive Perception, references to it remain in comments,
    hopefully can add later through settings, or detect when device has a
    large enough screen.
    
    Had to update many parts to work with toolbar, as for some reason cannot
    use layout to set something below the toolbar (it fails to work properly
    for some unknown reason) and the toolbar frequently hid key elements.

--- MobileDevelopmentCW_4A/app/src/main/java/com/example/chaos/mobiledevelopmentcw_4a/ListViewAdapter.java
+++ MobileDevelopmentCW_4A/app/src/main/java/com/example/chaos/mobiledevelopmentcw_4a/ListViewAdapter.java
@@ -25,27 +25,27 @@ public class ListViewAdapter extends BaseAdapter{
     /*** Declarations (Lists) ***/
     public static ArrayList<String> nameLst = new ArrayList<>();
     public static ArrayList<Integer> initLst = new ArrayList<>();
-    public static ArrayList<Integer> pasPerLst = new ArrayList<>();
+    /*public static ArrayList<Integer> pasPerLst = new ArrayList<>();*/
 
     /*** Declarations (Variables) ***/
     Activity context;
     String name[];
-    Integer pasPer[];
+    /*Integer pasPer[];*/
     Integer init[];
     int posPlus, posMinus;
     boolean plus, minus;
 
     /*** Get and Set passed values ***/
-    public ListViewAdapter(Activity context, String[] name, Integer[] pasPer, Integer[] init, ArrayList<String> nameLst, ArrayList<Integer> initLst, ArrayList<Integer> pasPerLst)
+    public ListViewAdapter(Activity context, String[] name/*, Integer[] pasPer*/, Integer[] init, ArrayList<String> nameLst, ArrayList<Integer> initLst/*, ArrayList<Integer> pasPerLst*/)
     {
         super();
         this.context = context;
         this.name = name;
-        this.pasPer = pasPer;
+        /*this.pasPer = pasPer;*/
         this.init = init;
         this.nameLst = nameLst;
         this.initLst = initLst;
-        this.pasPerLst = pasPerLst;
+        /*this.pasPerLst = pasPerLst;*/
     }
 
     public int getCount()
@@ -65,7 +65,7 @@ public class ListViewAdapter extends BaseAdapter{
     private class ViewHolder
     {
         /*** Declarations (UI) ***/
-        TextView txtName, txtPasPer;
+        TextView txtName/*, txtPasPer*/;
         EditText txtInitiative;
     }
 
@@ -80,7 +80,7 @@ public class ListViewAdapter extends BaseAdapter{
             convertView = inflater.inflate(R.layout.row, null);
             holder = new ViewHolder();
             holder.txtName = (TextView) convertView.findViewById(R.id.txtName);
-            holder.txtPasPer = (TextView) convertView.findViewById(R.id.txtPasPer);
+            /*holder.txtPasPer = (TextView) convertView.findViewById(R.id.txtPasPer);*/
             holder.txtInitiative= (EditText) convertView.findViewById(R.id.txtInitiative);
             convertView.setTag(holder);
         } else {
@@ -136,19 +136,19 @@ public class ListViewAdapter extends BaseAdapter{
     public void ActualAdd(ViewHolder holder, int position)
     {
         holder.txtName.setText(name[position]);
-        holder.txtPasPer.setText(pasPer[position].toString());
+        /*holder.txtPasPer.setText(pasPer[position].toString());*/
         holder.txtInitiative.setText(init[position].toString());
         if (minus)
         {
             holder.txtName.setText(name[posMinus]);
-            holder.txtPasPer.setText(pasPer[posMinus].toString());
+            /*holder.txtPasPer.setText(pasPer[posMinus].toString());*/
             holder.txtInitiative.setText(init[posMinus].toString());
             minus = false;
         }
         if (plus)
         {
             holder.txtName.setText(name[posPlus]);
-            holder.txtPasPer.setText(pasPer[posPlus].toString());
+            /*holder.txtPasPer.setText(pasPer[posPlus].toString());*/
             holder.txtInitiative.setText(init[posPlus].toString());
             plus = false;
         }
@@ -166,9 +166,9 @@ public class ListViewAdapter extends BaseAdapter{
         init[position] = init[posMinus];
         init[posMinus] = temp2;
 
-        temp3 = pasPer[position];
+        /*temp3 = pasPer[position];
         pasPer[position] = pasPer[posMinus];
-        pasPer[posMinus] = temp3;
+        pasPer[posMinus] = temp3;*/
     }
 
     public void SwitcherooPlus(int position, int posPlus)
@@ -183,23 +183,23 @@ public class ListViewAdapter extends BaseAdapter{
         init[position] = init[posPlus];
         init[posPlus] = temp2;
 
-        temp3 = pasPer[position];
+        /*temp3 = pasPer[position];
         pasPer[position] = pasPer[posPlus];
-        pasPer[posPlus] = temp3;
+        pasPer[posPlus] = temp3;*/
     }
 
     public void DeleteCombatant(int position)
     {
         nameLst.remove(position);
         initLst.remove(position);
-        pasPerLst.remove(position);
+        /*pasPerLst.remove(position);*/
 
         name = nameLst.toArray(new String[0]);
         init = initLst.toArray(new Integer[0]);
-        pasPer = pasPerLst.toArray(new Integer[0]);
+        /*pasPer = pasPerLst.toArray(new Integer[0]);*/
 
         MainMenu.nameArr = name;
         MainMenu.initArr = init;
-        MainMenu.pasPerArr = pasPer;
+        /*MainMenu.pasPerArr = pasPer;*/
     }
 }
--- MobileDevelopmentCW_4A/app/src/main/java/com/example/chaos/mobiledevelopmentcw_4a/MainMenu.java
+++ MobileDevelopmentCW_4A/app/src/main/java/com/example/chaos/mobiledevelopmentcw_4a/MainMenu.java
@@ -21,7 +21,9 @@ import java.util.Collections;
 
 public class MainMenu extends AppCompatActivity {
 
-    /*** Declarations (UI) ***/
+    /***
+     * Declarations (UI)
+     ***/
     Button but1, but2, but5, but6, but8;
     ListView lstVw1;
     ListViewAdapter lstVwAda;
@@ -29,7 +31,9 @@ public class MainMenu extends AppCompatActivity {
     TextView txtName, txtPasPer;
     private ViewFlipper switcher;
 
-    /*** Declarations (Lists) ***/
+    /***
+     * Declarations (Lists)
+     ***/
     public ArrayList<String> nameLst = new ArrayList<>();
     public ArrayList<Integer> initLst = new ArrayList<>();
     public ArrayList<Integer> pasPerLst = new ArrayList<>();
@@ -38,7 +42,9 @@ public class MainMenu extends AppCompatActivity {
     public ArrayList<Integer> tempPasPerLst = new ArrayList<>();
 
 
-    /*** Declarations (Variables) ***/
+    /***
+     * Declarations (Variables)
+     ***/
     static String[] nameArr;
     String[] tempNameArr;
     static Integer[] initArr, pasPerArr, tempInitArr, tempPasPerArr;
@@ -46,7 +52,9 @@ public class MainMenu extends AppCompatActivity {
     private String tempName;
     private Integer tempInit, tempPasPer;
 
-    /*** When the application loads ***/
+    /***
+     * When the application loads
+     ***/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,34 +69,27 @@ public class MainMenu extends AppCompatActivity {
 
         /*** Sets up all UI components ***/
         switcher = (ViewFlipper) findViewById(R.id.ViewFlipper);
-        but1 = (Button) findViewById(R.id.butAdd);
+        //but1 = (Button) findViewById(R.id.butAdd);
         but2 = (Button) findViewById(R.id.butSort);
         but5 = (Button) findViewById(R.id.butConfirm);
         but6 = (Button) findViewById(R.id.butCancel);
         but8 = (Button) findViewById(R.id.butInitTracker);
         txtName = (TextView) findViewById(R.id.txtName);
+/*
         txtPasPer = (TextView) findViewById(R.id.txtPasPer);
+*/
         txtInit = (EditText) findViewById(R.id.txtInitiative);
         name = (EditText) findViewById(R.id.editName);
         init = (EditText) findViewById(R.id.editInitiative);
-        pasPer = (EditText) findViewById(R.id.editPasPer);
+        //pasPer = (EditText) findViewById(R.id.editPasPer);
         lstVw1 = (ListView) findViewById(R.id.mainListView);
+
+        //Can we remove this from outside the Initiative Tracker?
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        /*** Switch to Add Combatant screen ***/
-        but1.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
                 ClearInputs();
                 if (count > 0) {
                     ArraysToList();
@@ -97,12 +98,24 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
-        /*** Sort all combatants by Initiative (High -> Low) ***/
-        but2.setOnClickListener(new View.OnClickListener()
-        {
+
+        /*** Switch to Add Combatant screen ***/
+        /*
+        but1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
+                ClearInputs();
+                if (count > 0) {
+                    ArraysToList();
+                }
+                switcher.showNext();
+            }
+        }); */
+
+        /*** Sort all combatants by Initiative (High -> Low) ***/
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 if (count > 1) {
                     SortEverything();
                 }
@@ -110,24 +123,16 @@ public class MainMenu extends AppCompatActivity {
         });
 
         /*** Return to Initiative Order - adding the combatant in the process ***/
-        but5.setOnClickListener(new View.OnClickListener()
-        {
+        but5.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                if (name == null)
-                {
+            public void onClick(View view) {
+                if (name == null) {
                     MissingName();
-                }
-                else if (init == null)
-                {
+                } else if (init == null) {
                     MissingInit();
-                }
-                else if (pasPer == null)
-                {
+                } /*else if (pasPer == null) {
                     MissingPasPer();
-                }
-                else {
+                }*/ else {
                     PrepForArrays();
                     switcher.showPrevious();
                     AddToArrays();
@@ -136,21 +141,17 @@ public class MainMenu extends AppCompatActivity {
         });
 
         /*** Return to main view without adding a combatant ***/
-        but6.setOnClickListener(new View.OnClickListener()
-        {
+        but6.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 switcher.showPrevious();
             }
         });
 
         /*** Goes to the initiative tracker screen ***/
-        but8.setOnClickListener(new View.OnClickListener()
-        {
+        but8.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 switcher.setDisplayedChild(switcher.indexOfChild(findViewById(R.id.TrackerView)));
             }
         });
@@ -178,37 +179,40 @@ public class MainMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*** Resets input fields ***/
-    public void ClearInputs()
-    {
+    /***
+     * Resets input fields
+     ***/
+    public void ClearInputs() {
         name.setText("");
         init.setText("");
-        pasPer.setText("");
+        /*pasPer.setText("");*/
     }
 
-    /*** Gets new data into Lists for adding to arrays ***/
-    public void PrepForArrays()
-    {
+    /***
+     * Gets new data into Lists for adding to arrays
+     ***/
+    public void PrepForArrays() {
         /*** Gets inputs and places them into temporary holders ***/
         tempName = name.getText().toString();
         tempInit = Integer.parseInt(init.getText().toString());
-        tempPasPer = Integer.parseInt(pasPer.getText().toString());
+        /*tempPasPer = Integer.parseInt(pasPer.getText().toString());*/
 
         /*** Adds the temporary holders into the relevant lists ***/
         nameLst.add(tempName);
         initLst.add(tempInit);
-        pasPerLst.add(tempPasPer);
+        /*pasPerLst.add(tempPasPer);*/
 
         /*** Overwrites arrays with List data ***/
         nameArr = nameLst.toArray(new String[0]);
         initArr = initLst.toArray(new Integer[0]);
-        pasPerArr = pasPerLst.toArray(new Integer[0]);
+        /*pasPerArr = pasPerLst.toArray(new Integer[0]);*/
     }
 
-    /*** Passes arrays into ListViewAdapter ***/
-    public void AddToArrays()
-    {
-        lstVwAda = new com.example.chaos.mobiledevelopmentcw_4a.ListViewAdapter(this, nameArr, pasPerArr, initArr, nameLst, initLst, pasPerLst);
+    /***
+     * Passes arrays into ListViewAdapter
+     ***/
+    public void AddToArrays() {
+        lstVwAda = new com.example.chaos.mobiledevelopmentcw_4a.ListViewAdapter(this, nameArr, /*pasPerArr,*/ initArr, nameLst, initLst/*, pasPerLst*/);
         lstVw1.setAdapter(lstVwAda);
 
         /*** Gets number of entries ***/
@@ -218,65 +222,65 @@ public class MainMenu extends AppCompatActivity {
         Toast.makeText(this, "Combatants present: " + count, Toast.LENGTH_SHORT).show();
     }
 
-    /*** Overwrites lists with Array data ***/
-    public void ArraysToList()
-    {
+    /***
+     * Overwrites lists with Array data
+     ***/
+    public void ArraysToList() {
         nameLst.clear();
         initLst.clear();
-        pasPerLst.clear();
+        /*pasPerLst.clear();*/
 
         nameLst = new ArrayList(Arrays.asList(nameArr));
         initLst = new ArrayList(Arrays.asList(initArr));
-        pasPerLst= new ArrayList(Arrays.asList(pasPerArr));
+        /*pasPerLst = new ArrayList(Arrays.asList(pasPerArr));*/
     }
 
-    /*** Displays a toast when called ***/
-    public void MissingName()
-    {
+    /***
+     * Displays a toast when called
+     ***/
+    public void MissingName() {
         Toast.makeText(this, "Please fill in the 'Name' section", Toast.LENGTH_SHORT).show();
     }
 
-    /*** Displays a toast when called ***/
-    public void MissingInit()
-    {
+    /***
+     * Displays a toast when called
+     ***/
+    public void MissingInit() {
         Toast.makeText(this, "Please fill in the 'Initiative' section", Toast.LENGTH_SHORT).show();
     }
 
-    /*** Displays a toast when called ***/
-    public void MissingPasPer()
-    {
+    /***
+     * Displays a toast when called
+     ***/
+    public void MissingPasPer() {
         Toast.makeText(this, "Please fill in the 'Passive Perception' section", Toast.LENGTH_SHORT).show();
     }
 
-    public void SortEverything()
-    {
+    public void SortEverything() {
         tempNameArr = Arrays.copyOf(nameArr, nameArr.length);
         tempInitArr = Arrays.copyOf(initArr, initArr.length);
-        tempPasPerArr = Arrays.copyOf(pasPerArr, pasPerArr.length);
+        /*tempPasPerArr = Arrays.copyOf(pasPerArr, pasPerArr.length);*/
 
         Arrays.sort(tempInitArr, Collections.<Integer>reverseOrder());
 
         tempNameLst.clear();
         tempInitLst.clear();
-        tempPasPerLst.clear();
+        /*tempPasPerLst.clear();*/
 
         int len1, len2;
 
         len1 = tempInitArr.length;
         len2 = initLst.size();
 
-        for (int i = 0; i < len1; i++)
-        {
-            for (int x = 0; x < len2; x++)
-            {
-                if (initLst.get(x) == tempInitArr[i])
-                {
+        for (int i = 0; i < len1; i++) {
+            for (int x = 0; x < len2; x++) {
+                if (initLst.get(x) == tempInitArr[i]) {
                     tempNameLst.add(nameLst.get(x));
-                    tempPasPerLst.add(pasPerLst.get(x));
+                    /*tempPasPerLst.add(pasPerLst.get(x));*/
 
                     initLst.remove(x);
                     nameLst.remove(x);
-                    pasPerLst.remove(x);
+                    /*pasPerLst.remove(x);*/
 
                     len1 = tempInitArr.length;
                     len2 = initLst.size();
@@ -285,7 +289,7 @@ public class MainMenu extends AppCompatActivity {
         }
 
         nameArr = tempNameLst.toArray(new String[0]);
-        pasPerArr = tempPasPerLst.toArray(new Integer[0]);
+        /*pasPerArr = tempPasPerLst.toArray(new Integer[0]);*/
         initArr = Arrays.copyOf(tempInitArr, tempInitArr.length);
 
         ArraysToList();
